254a9199170382fd20847accdedd11d5f2d13eda
Sergey Kupriyanov <Velzedor@yandex.ru>
2015-06-09 12:50:49 +0300

    rspec named routes fix

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,17 +7,17 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
-      visit root_path
+      visit home_path
       expect(page).to have_content('Sample App')
     end
 
     it "should have the right title" do
-      visit root_path
+      visit home_path
       expect(page).to have_title("#{base_title}")
     end
 
     it "should not have a custom page title" do
-      visit root_path
+      visit home_path
       expect(page).not_to have_title('Home')
     end
   end
